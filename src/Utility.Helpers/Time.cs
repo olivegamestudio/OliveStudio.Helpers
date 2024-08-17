aaafdcc6f5a505ebfd7354304ea0c2ad58be7273
Simon Jefferies <simon@olivegame.studio>
2024-08-17 22:03:44 +0100

    Imported.

---
+++ src/Utility.Helpers/Time.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Utility;
+
+/// <summary>
+/// Provides access to time-related functionality.
+/// </summary>
+public static class Time
+{
+    /// <summary>
+    /// Gets or sets the time that has elapsed since the last frame or update.
+    /// </summary>
+    public static TimeSpan DeltaTime { get; set; }
+}
